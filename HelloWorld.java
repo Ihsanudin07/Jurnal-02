f65bcc15142743d375cae709806e6cc70b1bc2e2
Ihsanudin07 <71365765+Ihsanudin07@users.noreply.github.com>
2020-09-16 12:18:40 +0700

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+   public static void main (String[] args){
+           System.out.println("Nama : Ihsanudin Pradana Putra");
+	   System.out.println("Nim  : 6706204050");
+	   System.out.println("Kelas : D3IF4404");
+	}
+}
+	
